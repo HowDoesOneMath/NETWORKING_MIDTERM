9883e7e4be9a0143fc4c200bba19830d77982801
Jakob Anderson <jakob.anderson@uoit.net>
2020-03-05 11:29:39 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ NETWORKING EXAM/Assets/Scripts/BasicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicManager : MonoBehaviour
+{
+    public GameObject realTank;
+    public GameObject dummyTank;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (NetworkingManager.MY_ID == 0)
+        {
+            NetworkingManager.tank1 = realTank;
+            NetworkingManager.tank2 = dummyTank;
+        }
+        else
+        {
+            NetworkingManager.tank2 = realTank;
+            NetworkingManager.tank1 = dummyTank;
+        }
+
+        NetworkingManager.bullet = bullet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NetworkingManager.ProcessPackets();
+    }
+}
