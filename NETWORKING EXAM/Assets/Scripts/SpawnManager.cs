9b72ea7f2694a50ebb28477858db42315c6066ee
Jakob Anderson <jakob.anderson@uoit.net>
2020-03-05 11:45:15 -0500

    New fix

---
+++ NETWORKING EXAM/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Vector3 s1;
+    public Vector3 s2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(NetworkingManager.tank1, s1, Quaternion.identity);
+        Instantiate(NetworkingManager.tank2, s1, Quaternion.AngleAxis(180, Vector3.up));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
