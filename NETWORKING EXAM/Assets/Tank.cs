8db00f5ddfe2fed23d5fc4fa1083effd9f2247d5
Audaxxy <nathan.boldy@insanitycorr.com>
2020-03-05 10:41:24 -0500

    Initial commit

---
+++ NETWORKING EXAM/Assets/Tank.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float rotate = 10.0f;
+
+	private void Update()
+	{
+
+		if (Input.GetKey(KeyCode.W))
+			transform.position += transform.forward * Time.deltaTime * speed;
+
+		if (Input.GetKey(KeyCode.S))
+			transform.position -= transform.forward * Time.deltaTime * speed;
+
+		if (Input.GetKey(KeyCode.D))
+			transform.Rotate(Vector3.up * Time.deltaTime * rotate);
+
+		if (Input.GetKey(KeyCode.A))
+			transform.Rotate(Vector3.down * Time.deltaTime * rotate);
+
+	}
+}
