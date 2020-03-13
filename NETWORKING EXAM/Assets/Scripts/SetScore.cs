17b0c1faf059e1b9da92026b616c2bcf91862f9e
Jakob Anderson <jakob.anderson@uoit.net>
2020-03-12 21:02:55 -0400

    It B done

---
+++ NETWORKING EXAM/Assets/Scripts/SetScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SetScore : MonoBehaviour
+{
+    Text myText;
+    int myID = -1;
+    static int compareID = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        compareID++;
+        myID = compareID;
+        myText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "P" + (myID + 1) + ": " + BasicManager.score[myID];
+    }
+}
